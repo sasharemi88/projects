3c0141603541ac0b164e1a53ac07ca41b94e0ea1
sasharemi88 <sasharemi88@gmail.com>
2020-10-19 11:50:43 +0500

    111

--- trip_adv/hotel_parser/hotel_parser/items.py
+++ trip_adv/hotel_parser/hotel_parser/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class TourObject(scrapy.Item):
+class Hotel(scrapy.Item):
     # define the fields for your item here like:
     region = scrapy.Field()
     city = scrapy.Field()
--- trip_adv/hotel_parser/hotel_parser/spiders/hotel_spider.py
+++ trip_adv/hotel_parser/hotel_parser/spiders/hotel_spider.py
@@ -1,20 +1,17 @@
 import scrapy
 from urllib.parse import urljoin
-from obj_parse.items import TourObject
+from obj_parse.items import Hotel
 
-class ObjectsSpider(scrapy.Spider):
-    name = 'ObjectsSpider'
+class HotelSpider(scrapy.Spider):
+    name = 'HotelSpider'
     allowed_domains = ["www.tripadvisor.ru"]
     start_urls = ["https://www.tripadvisor.ru/Restaurants-g2324049-Nizhny_Novgorod_Oblast_Volga_District.html"]
 
-    visited_urls = []
-    subcat = ['Рестораны', 'Быстрые перекусы', 'Фастфуд', 'Десерты', 'Кофе и чай', 'Бар', 'Паб', 'Кафе', 'Булочные', 'Гастропаб']
+    visited_urls = []    
     def parse(self, response):
-        top = 'Топ ресторан - ' +  response.xpath('//a[@class="_15_ydu6b"][1]/text()').get()
-        print(top)
         if response.url not in self.visited_urls:
             self.visited_urls.append(response.url)
-            for obj_link in response.xpath('//a[@class="_15_ydu6b"]/@href').extract():
+            for obj_link in response.xpath('//div[@class="listing_title"]/a[@class="property_title prominent "]/@href').extract():
                 url = urljoin("https://www.tripadvisor.ru", obj_link)
                 print(url)
                 yield response.follow(url, callback=self.parse_obj)
@@ -24,13 +21,11 @@ class ObjectsSpider(scrapy.Spider):
             yield response.follow(next_page_url, callback=self.parse)
 
     def parse_obj(self, response):
-        obj = TourObject()
+        obj = Hotel()
         obj["region"] = response.xpath('//li[@class="breadcrumb"][4]/a/span/text()').get()
         obj["city"] = response.xpath('//li[@class="breadcrumb"][5]/a/span/text()').get()
-        obj["category"] = 'Рестораны'
-        obj_tags = response.xpath('//a[@class="_2mn01bsa"]/text()').extract()
-        tags = [i for i in obj_tags if i in self.subcat]        
+        obj["category"] = 'Отели'
         obj["subcategory"] = ''
-        obj["tags"] = tags
-        obj["name"] =  response.xpath('//h1[@class="_3a1XQ88S"]/text()').get() 
+        obj["tags"] = ''
+        obj["name"] =  response.xpath('//h1[@class="_1mTlpMC3"]/text()').get() 
         yield obj     
