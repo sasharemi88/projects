66d99e9a5224c896830b1782b60c9bc770e09e58
sasharemi88 <sasharemi88@gmail.com>
2020-11-20 11:36:51 +0500

    20-11

--- trip_adv/test_id_search.py
+++ trip_adv/test_id_search.py
@@ -7,14 +7,6 @@ Created on Thu Nov 19 12:01:37 2020
 
 import requests
 import re
-import socket
 
-'''
-s = socket.socket()
-print(s.connect(('www.tripadvisor.ru', 433)))
-'''
-r = requests.get('https://www.tripadvisor.ru/Attraction_Review-g1439536-d2615844-Reviews-Khimki_Art_Gallery-Khimki_Moscow_Oblast_Central_Russia.html')
-#mask = re.compile("-d\d+")
-#id = mask.search(r)[0].replace('-d','')
-
-print(r.headers)
+r = requests.get('https://www.tripadvisor.ru/Attraction_Review-g1439536-d2615844-Reviews-Khimki_Art_Gallery-Khimki_Moscow_Oblast_Central_Russia.html', verify=True)
+print(r.status_code)
